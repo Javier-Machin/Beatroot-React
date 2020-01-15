1bc2b8d4656683c85dabd5f5cd1a187c2e869aa1
Javier M <javier_machin_acosta@hotmail.com>
2020-01-15 19:02:05 +0000

    Updates

---
+++ precache-manifest.40f1b7ccc23f4385ed69b93d851cfdca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757c9dde7d3827653d31736900c2fa25",
+    "url": "/music-beast-manager/index.html"
+  },
+  {
+    "revision": "7cf6c24153f1124ff1b0",
+    "url": "/music-beast-manager/static/css/main.a99242a9.chunk.css"
+  },
+  {
+    "revision": "67a6a369d25366d47d55",
+    "url": "/music-beast-manager/static/js/2.b2f729ce.chunk.js"
+  },
+  {
+    "revision": "e15ad7274e911b8f3ad618de33d87e12",
+    "url": "/music-beast-manager/static/js/2.b2f729ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7cf6c24153f1124ff1b0",
+    "url": "/music-beast-manager/static/js/main.ac585468.chunk.js"
+  },
+  {
+    "revision": "758987456f2fbf314bb1",
+    "url": "/music-beast-manager/static/js/runtime-main.92286565.js"
+  },
+  {
+    "revision": "7cecbe97ea0675b81270da462df1ea46",
+    "url": "/music-beast-manager/static/media/edit.7cecbe97.png"
+  },
+  {
+    "revision": "e1df52e22d765c46e54a61e7a5b73674",
+    "url": "/music-beast-manager/static/media/logout.e1df52e2.svg"
+  },
+  {
+    "revision": "ab36c95cf63f07682f2e262065f68b7f",
+    "url": "/music-beast-manager/static/media/lyrics.ab36c95c.png"
+  },
+  {
+    "revision": "f67b1dd7e362e553f1ababb27755564d",
+    "url": "/music-beast-manager/static/media/track.f67b1dd7.png"
+  }
+]);
